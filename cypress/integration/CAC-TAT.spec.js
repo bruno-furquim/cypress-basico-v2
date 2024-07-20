305c2770570ba3e641a367b2d82732b1fdaa6611
Bruno Furquim <bruno_furquim@msn.com>
2024-07-20 12:24:42 -0300

    Correção de erros

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -144,7 +144,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop'})
         .should(function ($input) {
             expect($input[0].files[0].name).to.equal('example.json')
-            expect($input[0].files[0].type).to.equal('aplication/json')
+            expect($input[0].files[0].type).to.equal('application/json')
         })
     });
 
